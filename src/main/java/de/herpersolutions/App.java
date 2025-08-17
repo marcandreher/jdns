657a000779211feb2bed6fe263a5bc5b4f40ac63
marcandreher <inbox@osu-server-list.com>
2025-08-17 12:04:50 +0000

    init: jdns

---
+++ src/main/java/de/herpersolutions/App.java
@@ -0,0 +1,17 @@
+package de.herpersolutions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * JDNS Standalone Server
+ *
+ */
+public class App 
+{
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        logger.info( "Hello World!" );
+    }
+}
